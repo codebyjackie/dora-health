32a0a8e7376e6c3f15cf44346d805f7e73411ac0
codebyjackie <JackieLinDev@foxmail.com>
2024-05-27 17:54:59 -0500

    chore: Initial commit
    - Initialized project directory
    - Configured various configuration files

---
+++ README.md
@@ -0,0 +1,2 @@
+# dora-health
+A Vue.js application for tracking diet, calculating various fitness metrics, and providing a comprehensive focus tool.
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>app.vue</div>
+  <RouterView />
+  <RouterLink to="/">Home</RouterLink>
+</template>
+
+<style scoped></style>
